574aa508584bcf7d175db40253b1ffe6cc493e93
Ira <dudairocka@gmail.com>
2025-05-09 17:38:04 +0700

    fffff

--- task1.py
+++ task1.py
@@ -9,14 +9,29 @@
 # 2.Вернуть список вершин в порядке их посещения.
 
 # Пример входных данных
-# graph = {
-#     1: [2, 3],
-#     2: [1, 4],
-#     3: [1, 5],
-#     4: [2],
-#     5: [3]
-# }
-# start = 1
-#
 # Пример выходных данных
 # [1, 2, 4, 3, 5]  # Возможен и другой порядок, зависящий от реализации DFS
+graph = {
+1: [2, 3],
+2: [1, 4],
+3: [1, 5],
+4: [2],
+5: [3]}
+first = 1
+Suda=[first]
+Already=set() #посещнные
+Result=[] #это показываем
+while Suda:
+    now=Suda.pop() #извлекаем с конца
+    if now not in Already:
+        Already.add(now)
+        Smeshznie = graph.get(now, []) #соседи рассматриваемой (добавляем в обратном порядке чтобы сначала посмотреть двойку)
+        #print(Smeshznie)
+        Result.append(now)
+        #print(now)
+        for Smeshznie in reversed(Smeshznie):
+            if Smeshznie not in Already: #непосещенные соседи
+                Suda.append(Smeshznie)
+print(Result)
+
+
